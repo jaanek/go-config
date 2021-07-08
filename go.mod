431b00aab424ba3a914a0998f85f91c872ccbbb1
Jaanek Oja <jaanekoja@gmail.com>
2021-07-08 10:01:38 +0300

    read from env also

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jaanek/go-config
 
 go 1.16
 
-require gopkg.in/yaml.v2 v2.4.0 // indirect
+require (
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0
+)
--- yamlcfg/yamlcfg.go
+++ yamlcfg/yamlcfg.go
@@ -5,29 +5,43 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/kelseyhightower/envconfig"
 	"gopkg.in/yaml.v2"
 )
 
+// based on https://dev.to/ilyakaznacheev/a-clean-way-to-pass-configs-in-a-go-application-1g64
 func NewConfig(configPath string, config interface{}) error {
+	err := readFile(configPath, config)
+	if err != nil {
+		return err
+	}
+	return readEnv(configPath, config)
+}
+
+func readFile(configPath string, config interface{}) error {
 	file, err := os.Open(configPath)
 	if err != nil {
-            return err
+		return err
 	}
 	defer file.Close()
 	d := yaml.NewDecoder(file)
-	if err := d.Decode(&config); err != nil {
-            return err
+	if err := d.Decode(config); err != nil {
+		return err
 	}
 	return nil
+}
+
+func readEnv(configPath string, config interface{}) error {
+	return envconfig.Process("", config)
 }
 
 func ValidateConfigPath(path string) error {
 	s, err := os.Stat(path)
 	if err != nil {
-            return err
+		return err
 	}
 	if s.IsDir() {
-            return fmt.Errorf("'%s' is a directory, not a file", path)
+		return fmt.Errorf("'%s' is a directory, not a file", path)
 	}
 	return nil
 }
@@ -37,7 +51,7 @@ func ParseFlags() (string, error) {
 	flag.StringVar(&configPath, "config", "./config.yml", "path to config file")
 	flag.Parse()
 	if err := ValidateConfigPath(configPath); err != nil {
-            return "", err
+		return "", err
 	}
 	return configPath, nil
 }
